03c703e55ff49937ecb3080b01d3240e32cdcaf5
GLOBANT\luis.saldarriaga <luis.saldarriaga@globant.com>
2017-10-25 14:33:00 -0500

    Integración con template

--- src/main/webapp/app/app.module.ts
+++ src/main/webapp/app/app.module.ts
@@ -52,7 +52,7 @@ import {
         AppRealStateAdminModule,
         AppRealStateAccountModule,
         AppRealStateEntityModule,
-        //BrowserAnimationsModule,
+        BrowserAnimationsModule,
         FormsModule,
         HttpModule,
         TranslateModule.forRoot({
